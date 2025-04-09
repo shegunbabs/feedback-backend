2d6d3df14dd1869c6656f920a74a83c378c538f3
Shegun Babs <shegun.babs@gmail.com>
2025-04-09 14:21:16 -0400

    rf: added models, factories n migrations

---
+++ database/factories/FeedbackFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Feedback;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Feedback>
+ */
+class FeedbackFactory extends Factory
+{
+    protected $model = Feedback::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'customer_name' => $this->faker->name(),
+            'message' => $this->faker->realText(),
+            'rating' => $this->faker->numberBetween(1, 5),
+        ];
+    }
+}
